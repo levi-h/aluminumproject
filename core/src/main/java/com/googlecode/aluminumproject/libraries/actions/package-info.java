f791faad23203da89fc53abc23f4540def5e44b6
levi_h <levi_h@localhost>
2011-10-20 16:00:57 +0200

    added package information for the API docs

---
+++ core/src/main/java/com/googlecode/aluminumproject/libraries/actions/package-info.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2011 Levi Hoogenberg
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * The interfaces and factories for {@link com.googlecode.aluminumproject.libraries.actions.Action actions} and {@link
+ * com.googlecode.aluminumproject.libraries.actions.ActionContribution action contributions}. For more information on
+ * these subjects, please see the <a href="http://code.google.com/p/aluminumproject/wiki/Actions">action</a> and <a
+ * href="http://code.google.com/p/aluminumproject/wiki/ActionContributions">action contribution</a> wiki pages.
+ */
+package com.googlecode.aluminumproject.libraries.actions;
